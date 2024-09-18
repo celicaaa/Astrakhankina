5c87ef3e992b45ee45c183e15079b5238e74ab1c
celicaaa <la-astrahankina@yandex.ru>
2024-09-18 23:55:45 +0300

    создание меню

--- Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba.cpp
+++ Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba.cpp
@@ -2,19 +2,73 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
+struct Pipe {
+
+    string name = "None";
+    float length = 0.0;
+    int diameter = 0;
+    bool remont = false;
+
+};
 
 int main()
-{
-    std::cout << "Hello World xaxaxa!\n";
+{   
+    Pipe pipe0;
+    int n;
+    while (true) {
+        cout << endl << "Menu:" << endl;
+        cout << "1 Add pipe" << endl;
+        cout << "2 Add CS" << endl;
+        cout << "3 View all objects" << endl;
+        cout << "4 Edit pipe" << endl;
+        cout << "5 Edit CS" << endl;
+        cout << "6 Save" << endl;
+        cout << "7 Download" << endl;
+        cout << "0 Exit" << endl;
+        cout << endl << "Enter the command number: ";
+        cin >> n;
+
+        if (cin.fail() || n < 0 || n > 7)
+        {
+            cout << "Not found this command" << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
+            continue;
+        }
+
+        switch (n)
+        {
+        case 1: //add pipe
+        {
+
+            cout << "iwed" << endl;
+            break;
+        }
+        case 2: //add cs
+        {
+            cout << "iwed" << endl;
+            break;
+        }
+        case 3: //edit pipe
+        {
+            cout << "jdku" << endl;
+            break;
+        }
+        case 4: //edit cs
+        {
+            cout << "jeiw" << endl;
+            break;
+        }
+        case 5: //save
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        }
+    }
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
