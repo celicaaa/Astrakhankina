e25177185f1f75fed7e8c09d0e2622d0ca9e0198
celicaaa <la-astrahankina@yandex.ru>
2024-09-19 15:14:53 +0300

    добавление трубы с параметрами и их проверка

--- Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba.cpp
+++ Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba/Astrakhankina_laba.cpp
@@ -7,11 +7,94 @@ using namespace std;
 struct Pipe {
 
     string name = "None";
-    float length = 0.0;
+    double length = 0.0;
     int diameter = 0;
     bool remont = false;
 
 };
+
+int check_int(int& int_data)
+
+{
+    cin >> int_data;
+    while (cin.fail() || cin.peek() != '\n' || int_data <= 0)
+
+    {
+        cin.clear();
+        cin.ignore(100000, '\n');
+        cout << "\nPlease, enter an int type > 0\n";
+        cin >> int_data;
+    }
+    return int_data;
+}
+double check_double(double& double_data)
+{
+    cin >> double_data;
+    while (cin.fail() || cin.peek() != '\n' || double_data <= 0)
+
+    {
+        cin.clear();
+        cin.ignore(100000, '\n');
+        cout << "\nPlease, enter a double type > 0\n";
+        cin >> double_data;
+
+    }
+    return double_data;
+
+}
+
+bool check_bool(bool& bool_data)
+
+{
+    cin >> bool_data;
+    while (cin.fail() || cin.peek() != '\n')
+    {
+        cin.clear();
+        cin.ignore(100000, '\n');
+        cout << "\nPlease, enter a bool type\n";
+        cin >> bool_data;
+
+    }
+    return bool_data;
+}
+Pipe AddPipe()
+
+{
+
+    Pipe new_pipe;
+    cout << endl << "Adding a new pipe..." << endl;
+    cout << "Enter the name of the pipe: ";
+    cin>>new_pipe.name;
+
+    cout << "Enter the length of the pipe: ";
+    check_double(new_pipe.length);
+
+    cout << "Enter the pipe diameter: ";
+    check_int(new_pipe.diameter);
+
+    cout << "Enter the repair status: ";
+    check_bool(new_pipe.remont);
+
+    return new_pipe;
+
+}
+
+void PrintAddPipe(Pipe& new_pipe)
+
+{
+    cout << endl << "Info about your pipe..." << endl;
+    if (new_pipe.name == "None")
+    {
+        cout << "No pipes available!\n";
+    }
+    else
+    {
+        cout << "Name: " << new_pipe.name << "\tLength: " << new_pipe.length
+
+            << "\tDiameter: " << new_pipe.diameter << "\tRepair: " << new_pipe.remont << endl;
+    }
+}
+
 
 int main()
 {   
@@ -42,8 +125,8 @@ int main()
         {
         case 1: //add pipe
         {
-
-            cout << "iwed" << endl;
+            pipe0 = AddPipe();
+            PrintAddPipe(pipe0);
             break;
         }
         case 2: //add cs
@@ -61,7 +144,22 @@ int main()
             cout << "jeiw" << endl;
             break;
         }
-        case 5: //save
+        case 5: 
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 6: //save
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 7: 
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 0: //exit
         {
             cout << "slwo" << endl;
             break;
